919424be7febc96bd196a4867eee64743ab75f93
Travis CI User <travis@example.org>
2018-07-05 03:44:42 +0000

    Update documentation

---
+++ implementors/core/fmt/trait.Debug.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors["lando"] = [{text:"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/fmt/trait.Debug.html\" title=\"trait core::fmt::Debug\">Debug</a> for <a class=\"enum\" href=\"lando/enum.Body.html\" title=\"enum lando::Body\">Body</a>",synthetic:false,types:["lando::body::Body"]},{text:"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/fmt/trait.Debug.html\" title=\"trait core::fmt::Debug\">Debug</a> for <a class=\"enum\" href=\"lando/enum.PayloadError.html\" title=\"enum lando::PayloadError\">PayloadError</a>",synthetic:false,types:["lando::http::PayloadError"]},{text:"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/fmt/trait.Debug.html\" title=\"trait core::fmt::Debug\">Debug</a> for <a class=\"struct\" href=\"lando/request/struct.RequestContext.html\" title=\"struct lando::request::RequestContext\">RequestContext</a>",synthetic:false,types:["lando::request::RequestContext"]},{text:"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/fmt/trait.Debug.html\" title=\"trait core::fmt::Debug\">Debug</a> for <a class=\"struct\" href=\"lando/request/struct.Identity.html\" title=\"struct lando::request::Identity\">Identity</a>",synthetic:false,types:["lando::request::Identity"]},];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
---
+++ implementors/failure/trait.Fail.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors["lando"] = [{text:"impl Fail for <a class=\"enum\" href=\"lando/enum.PayloadError.html\" title=\"enum lando::PayloadError\">PayloadError</a>",synthetic:false,types:["lando::http::PayloadError"]},];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
